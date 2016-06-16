4a64de0f261c4e1eb8f72a3000cd547e08233ae7
Jason Peak <jpeak5@lsu.edu>
2016-06-16 14:01:26 -0500

    begins 1.x: replace 0.x code with the new...

--- roles/java_components/files/gsearch-javaopts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's#JAVA_OPTS="-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC"#JAVA_OPTS="-Djava.awt.headless=true -Xmx1024m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -Dkakadu.home=/usr/local/djatoka/bin/Linux-x86-64 -Djava.library.path=/usr/local/djatoka/lib/Linux-x86-64 -DLD_LIBRARY_PATH=/usr/local/djatoka/lib/Linux-x86-64"#g' /etc/default/tomcat7
